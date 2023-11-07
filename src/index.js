72f144ff43396a68dff7d3ecc3591680b4b91b21
hamidreza8968 <hamidreza.seydabadi68@gmail.com>
2023-11-07 09:18:49 +0100

    edit: subscription of "AccountOperations" and "BalanceDisplay" in redux store

--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {Provider} from "react-redux";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 import store from "./storeV2";
 
-store.dispatch({type: "account/deposit" , payload:9000});
-console.log(store.getState());
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+      <Provider store={store}>
+          <App />
+      </Provider>
   </React.StrictMode>
 );
 
