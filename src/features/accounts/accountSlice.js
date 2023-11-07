650bbf4a2630c460df4739755e787bca1ab10e1c
hamidreza8968 <hamidreza.seydabadi68@gmail.com>
2023-11-07 11:54:30 +0100

    edit: using "Thunk" for data fetching (currency rate) in deposit functionality.

--- src/features/accounts/accountSlice.js
+++ src/features/accounts/accountSlice.js
@@ -1,34 +1,50 @@
 const initialStateAccount = {
-    balance : 0,
-    loan : 0,
-    loanPurpose : "",
+    balance: 0,
+    loan: 0,
+    loanPurpose: "",
 };
 
-export default function accountReducer(state=initialStateAccount , action) {
-    switch(action.type){
+export default function accountReducer(state = initialStateAccount, action) {
+    switch (action.type) {
         case "account/deposit":
-            return{...state, balance: state.balance + action.payload};
+            return {...state, balance: state.balance + action.payload};
         case "account/withdraw":
-            return{...state, balance: state.balance - action.payload};
+            return {...state, balance: state.balance - action.payload};
         case "account/requestLoan":
             if (state.loan > 0) return state;
-            return{...state, balance: state.balance + action.payload.amount, loan: action.payload.amount, loanPurpose: action.payload.purpose};
+            return {
+                ...state,
+                balance: state.balance + action.payload.amount,
+                loan: action.payload.amount,
+                loanPurpose: action.payload.purpose
+            };
         case "account/payLoan":
-            return{...state, loan: 0, loanPurpose: "", balance: state.balance - state.loan};
+            return {...state, loan: 0, loanPurpose: "", balance: state.balance - state.loan};
         default:
             return state;
     }
 }
 
-export function deposit(amount) {
-    return {type:"account/deposit", payload:amount}
+export function deposit(amount, currency) {
+    if (currency === "USD") return {type: "account/deposit", payload: amount};
+    return async function (dispatch, getState) {
+        const res = await fetch(`https://api.frankfurter.app/latest?amount=${amount}&from=${currency}&to=USD`);
+        const data = await res.json();
+        const converted = data.rates.USD;
+
+        dispatch({type: "account/deposit", payload: converted});
+
+    }
 }
+
 export function withdraw(amount) {
-    return {type:"account/withdraw", payload:amount}
+    return {type: "account/withdraw", payload: amount}
 }
-export function requestLoan(amount , purpose) {
-    return {type:"account/requestLoan", payload:{amount, purpose}}
+
+export function requestLoan(amount, purpose) {
+    return {type: "account/requestLoan", payload: {amount, purpose}}
 }
+
 export function payLoan() {
-    return {type:"account/payLoan"}
+    return {type: "account/payLoan"}
 }
